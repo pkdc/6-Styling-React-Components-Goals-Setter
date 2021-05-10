247b7e91c709fbc275ab9f42666c5664155670c9
pkdc <pkdchoy@gmail.com>
2021-05-10 23:31:52 +0100

    switched to set CSS Classes Dynamically instead of Setting Dynamic Inline Styles. Installed Styled Components.

--- src/components/CourseGoals/CourseInput/CourseInput.js
+++ src/components/CourseGoals/CourseInput/CourseInput.js
@@ -27,14 +27,10 @@ const CourseInput = (props) => {
 
   return (
     <form onSubmit={formSubmitHandler}>
-      <div className="form-control">
-        <label style={{ color: !isValid ? "red" : "black" }}>Course Goal</label>
+      <div className={`form-control ${!isValid ? 'invalid' : ''}`}>
+        <label>Course Goal</label>
         <input
           type="text"
-          style={{
-            backgroundColor: !isValid ? "salmon" : "transparent",
-            borderColor: !isValid ? "red" : "black",
-          }}
           onChange={goalInputChangeHandler}
         />
       </div>
